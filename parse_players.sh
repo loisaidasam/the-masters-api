fe9c78401d960561528d4036e066e7c9d8d4af97
loisaidasam <sam.sandberg@gmail.com>
2019-04-11 10:45:07 -0400

    `parse_players.sh` updates for 2019

--- parse_players.sh
+++ parse_players.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-pup '.leaderboard-table tbody tr.player-overview json{}' | \
-    jq '[.[] | {
-        player: .children[2].children[1].text,
+pup_path='.competitors tbody tr.Table2__tr json{}'
+
+# Ignore the first (empty) row
+/home/sam/bin/pup "$pup_path" | \
+    /home/sam/bin/jq '.[1:]' | \
+    /home/sam/bin/jq '[.[] | {
+        player: .children[1].children[1].text,
         pos: .children[0].text,
-        r1: .children[6].text,
-        r2: .children[7].text,
-        r3: .children[8].text,
-        r4: .children[9].text,
-        tot: .children[10].text,
-        to_par: .children[3].text,
-        today: .children[4].text,
-        thru: .children[5].text,
-    #     start: .children[1].text,
-    #     ctry: .children[2].children[0].src
+        r1: .children[5].text,
+        r2: .children[6].text,
+        r3: .children[7].text,
+        r4: .children[8].text,
+        tot: .children[9].text,
+        to_par: .children[2].text,
+        today: .children[3].text,
+        thru: .children[4].text,
+        link: .children[1].children[1].href,
+        country_flag_image: .children[1].children[0].src
     }]'
+
