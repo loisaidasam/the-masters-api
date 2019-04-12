52fb6c06561debf4bc6b5c7e869de7f395bc287f
loisaidasam <sam.sandberg@gmail.com>
2019-04-12 10:24:18 -0400

    Fix remaining parser changes as per #2

--- parse_players.sh
+++ parse_players.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+bin_dir="/home/sam/bin"
+
 pup_path='.competitors tbody tr.Table2__tr json{}'
 
+# Debugging
+# "$bin_dir/pup" "$pup_path" | \
+#     "$bin_dir/jq" '.[1:]' | "$bin_dir/jq" '.[0]'
+# exit 0
+
 # Ignore the first (empty) row
-/home/sam/bin/pup "$pup_path" | \
-    /home/sam/bin/jq '.[1:]' | \
-    /home/sam/bin/jq '[.[] | {
-        player: .children[2].children[1].text,
+"$bin_dir/pup" "$pup_path" | \
+    "$bin_dir/jq" '.[1:]' | \
+    "$bin_dir/jq" '[.[] | {
         pos: .children[0].text,
-        r1: .children[5].text,
-        r2: .children[6].text,
-        r3: .children[7].text,
-        r4: .children[8].text,
-        tot: .children[9].text,
-        to_par: .children[3].text,
-        today: .children[3].text,
-        thru: .children[4].text,
+        player: .children[2].children[1].text,
+        country_flag_image: .children[2].children[0].src,
         link: .children[2].children[1].href,
-        country_flag_image: .children[2].children[0].src
+        to_par: .children[3].text,
+        today: .children[4].text,
+        thru: .children[5].text,
+        r1: .children[6].text,
+        r2: .children[7].text,
+        r3: .children[8].text,
+        r4: .children[9].text,
+        tot: .children[10].text,
     }]'
